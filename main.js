702a7ffd749c8e6873d92e97f01496d56047f9d3
StasivPavlo <pavlostasiv@gmail.com>
2023-08-29 23:18:11 +0300

    refactored

--- main.js
+++ main.js
@@ -7,24 +7,50 @@ const component = requer('./component.json');
 
 const firstLaterToUpperCase = (str) => {
   return str.charAt(0).toUpperCase() + str.slice(1);
+};
+
+const getSelectOptions = (options) => {
+  return options.map(option => {
+    return `${option.label}: \`${option.value}\``;
+  }).join(', ');
+};
+
+const getPropertyTypeHandler = (property) => {
+  if (property.type === 'select') {
+    return firstLaterToUpperCase(property.type) + ' ' + `[${ getSelectOptions(property.options) }]`;
+  }
+
+  return firstLaterToUpperCase(property.type);
+};
+
+const getDefaultValue = (property) => {
+  switch (property.type) {
+    case 'color': return property.defaultValue ? `\"${property.defaultValue}\"` : '';
+    case 'text': return property.defaultValue ? `\"${property.defaultValue}\"` : '';
+    case 'number': return property.defaultValue ? `${property.defaultValue}` : '';
+    case 'checkbox': return property.defaultValue ? `\`${property.defaultValue}\`` : '\`false\`';
+    case 'select':
+      const selectedOption = property.options.find(option => option.value === property.defaultValue);
+
+      return `${selectedOption.label}: \`${selectedOption.value}\``;
+    default: return property.defaultValue;
+  }
 }
 
 const getProperty = (property) => {
   return [
     `${property.label}: \`${property.name}\``,
-    firstLaterToUpperCase(property.type),
-    property.defaultValue || ' ',
+    getPropertyTypeHandler(property),
+    getDefaultValue(property),
     property.hasLogicHandler ? property.handlerLabel : ' ',
-    !!property.dataBinding,
-    !!property.showInSettings,
+    property.dataBinding ? 'YES' : 'NO',
+    property.showInSettings ? 'YES' : 'NO',
     property.settingTooltip
   ]
-}
+};
 
 const columns = ['Property', 'Type', 'Default Value', 'Logic', 'Data Binding', 'UI Setting', 'Description'];
 
 const rows = component.properties.map(property => getProperty(property));
-
-markdownTable([columns, ...rows]);
 
 fs.writeFileSync('table.md', markdownTable([columns, ...rows]));
